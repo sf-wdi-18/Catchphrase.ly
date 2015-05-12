0e26d189b2aceb7e97d50a7647f669e38ed77112
Ilias Tsangaris <iliastsangaris@gmail.com>
2015-05-11 22:44:31 -0700

    starting point

--- index.js
+++ index.js
@@ -3,13 +3,19 @@ var express = require("express"),
     app = express(),
     path = require("path");
 
+// serve js & css files into a public folder
+app.use(express.static(__dirname + '/public'));
+
 // food index
-var foods = [];
+var foods =[
+  "Sushiritto", "Green Eggs & Ham", "Crayfish",
+  "Foie Gras", "Taco", "Kale"
+];
 
 // root path
 app.get("/", function(req, res){
   // render index.html
-  res.sendFile(path.join(__dirname + '/assets/views/index.html'));
+  res.sendFile(path.join(__dirname + '/public/views/index.html'));
 });
 
 // listen on port 3000
---
+++ public/js/app.js
@@ -0,0 +1,1 @@
+// Ajax & underscore templating
