910ef2ab6e29cbcc185a64df02a3af7b8e784013
Ilias Tsangaris <iliastsangaris@gmail.com>
2015-05-11 23:25:43 -0700

    ajax, no underscore templating

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,31 @@
+// Code Execution
+
 // on page load
 $(function(){
-
-  
-
+  //ajax to foods index
+  $.get("/foods", function(res){ 
+    // parse the response
+    foods = JSON.parse(res);
+    // render the results
+    View.render(foods, "foods");
+  })
 });
+
+
+
+// Code Definition
+
+// a view object
+function View() { };
+// a 'class method' aka static method for the View object
+View.render = function(items, id) {
+   // find the target partent by the id passed in
+  var targetParent = $("#" + id);
+  // iterate through the collection
+  items.forEach(function(item){
+    // build each list item
+    var child = $("<li class='list-group-item'>" + item + "</li>");
+    // append the child to the parent
+    targetParent.append(child);
+  });
+}
