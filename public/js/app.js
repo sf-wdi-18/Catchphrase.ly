b22b007b958835f76da65f676f52f161e5c4aacd
Ilias Tsangaris <iliastsangaris@gmail.com>
2015-05-12 00:42:07 -0700

    underscore templating

--- public/js/app.js
+++ public/js/app.js
@@ -5,9 +5,9 @@ $(function(){
   //ajax to foods index
   $.get("/foods", function(res){ 
     // parse the response
-    foods = JSON.parse(res);
+    var foods = JSON.parse(res);
     // render the results
-    View.render(foods, "foods");
+    View.render(foods, "food-ul", "foods-template");
   })
 });
 
@@ -16,16 +16,15 @@ $(function(){
 // Code Definition
 
 // a view object
-function View() { };
+function View() {};
 // a 'class method' aka static method for the View object
-View.render = function(items, id) {
-   // find the target partent by the id passed in
-  var targetParent = $("#" + id);
-  // iterate through the collection
-  items.forEach(function(item){
-    // build each list item
-    var child = $("<li class='list-group-item'>" + item + "</li>");
-    // append the child to the parent
-    targetParent.append(child);
-  });
-}
+View.render = function(items, parentId, templateId) {
+  // get food template
+  var foodTemplate = _.template($("#" + templateId).html());
+  // input data into template and append to parent
+  $("#" + parentId).html(foodTemplate({foods: items}));
+};
+// target a parent and clear everything inside
+View.clear = function(parentId) {
+  $("#" + parentId).html("");
+};
