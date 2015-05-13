c20bf4ab85b56e7fa29cd9298b06838a4e87114c
Ilias Tsangaris <iliastsangaris@gmail.com>
2015-05-12 19:19:34 -0700

    turned array of strings to array of objects

--- index.js
+++ index.js
@@ -1,30 +1,52 @@
 // requirements
 var express = require("express"),
     app = express(),
-    path = require("path");
+    path = require("path"),
+    bodyParser = require("body-parser");
 
 // serve js & css files into a public folder
 app.use(express.static(__dirname + '/public'));
 
+// get post data from
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // food index
 var foods =[
-  "Sushiritto", "Green Eggs & Ham", "Crayfish",
-  "Foie Gras", "Taco", "Kale"
+  {name: "Sushiritto", yumminess: "quite"},
+  {name: "Green Eggs & Ham", yumminess: "sure"},
+  {name: "Crayfish", yumminess: "depending"},
+  {name: "Foie Gras", yumminess: "omg"},
+  {name: "Kale", yumminess: "meh"}
 ];
 
 // root path
-app.get("/", function(req, res){
+app.get("/", function (req, res){
   // render index.html
   res.sendFile(path.join(__dirname + '/public/views/index.html'));
 });
 
 // foods index path
-app.get("/foods", function(req, res){
+app.get("/foods", function (req, res){
   // render foods index as JSON
   res.send(JSON.stringify(foods));
 });
 
+app.post("/foods", function (req, res){
+  // find new food in the req.body (thanks body parser)
+  var newFood = req.body;
+  console.log(req.body);
+  // add it to our food array
+  // foods.push(newFood);
+  // render the created object as json
+  res.send(newFood);
+});
+
+app.delete("/foods", function (req, res){
+  //delete food from the array
+})
+
 // listen on port 3000
-app.listen(3000, function(){
+app.listen(3000, function (){
   console.log("listening on port 3000");
 });
--- public/js/app.js
+++ public/js/app.js
@@ -1,21 +1,14 @@
-// Code Execution
-
 // on page load
 $(function(){
-  //ajax to foods index
-  $.get("/foods", function(res){ 
-    // parse the response
-    var foods = JSON.parse(res);
-    // render the results
-    View.render(foods, "food-ul", "foods-template");
-  })
+  // get and render the food
+  Food.all();
+  // initialize the view
+  View.init();
 });
 
+// // // // // // //
 
-
-// Code Definition
-
-// a view object
+// VIEW OBJECT
 function View() {};
 // a 'class method' aka static method for the View object
 View.render = function(items, parentId, templateId) {
@@ -24,7 +17,33 @@ View.render = function(items, parentId, templateId) {
   // input data into template and append to parent
   $("#" + parentId).html(template({collection: items}));
 };
-// target a parent and clear everything inside
-View.clear = function(parentId) {
-  $("#" + parentId).html("");
+View.clear = function(id) {
+  // clear everything inside an element
+  $("#" + id).html("");
 };
+View.init = function() {
+  // food form submit event listener
+  $("#food-form").on("submit", function(e){
+    // stop page reload
+    e.preventDefault();
+    // format form data into a query string
+    var foodParams = $(this).serialize();
+    Food.create(foodParams);
+  });
+}
+
+// FOOD OBJECT
+function Food() {};
+Food.all = function() {
+  $.get("/foods", function(res){ 
+    // parse the response
+    var foods = JSON.parse(res);
+    // render the results
+    View.render(foods, "food-ul", "foods-template");
+  });
+}
+Food.create = function(foodParams) {
+  $.post("/foods", foodParams).done(function(res){
+    console.log(res);
+  });
+}
